7d61a83dd3e0417cf50be5dbf8ebe7a5e89bbc4c
shiyaamv <151559543+shiyaamv@users.noreply.github.com>
2024-09-28 14:53:16 +0530

    Delete DA-10d.R

--- DA-10d.R
+++
@@ -1,9 +0,0 @@
-# Load the ggplot2 package
-library(ggplot2)
-# Box plot of Sepal Length for each Species
-ggplot(data = iris, aes(x = Species, y = Sepal.Length, fill = Species)) +
-  geom_boxplot() + # Adds box plot
-  labs(title = "Box Plot of Sepal Length by Species",
-       x = "Species",
-       y = "Sepal Length (cm)") +
-  theme_minimal()
